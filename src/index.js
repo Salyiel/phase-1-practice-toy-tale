d299b39ccb4f99947c29c1922c2e0874cff5e589
Stacy <salyiela@icloud.com>
2024-04-16 11:47:30 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-let addToy = false;
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+let addToy = false
+const createToyForm = document.querySelector('.add-toy-form')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +17,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const initialize = () => {
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toys => toys.forEach(showToy))
+}
+
+const showToy = toy => {
+  let toyNew = toy
+  let toyCard = document.createElement('div')
+  toyCard.className = 'card'
+  toyCard.innerHTML = `
+  <h2>${toyNew.name}</h2>
+  <img src=${toyNew.image} class="toy-avatar" />`
+  let likesP = document.createElement('p')
+  likesP.innerText = `${toyNew.likes} Likes`
+  toyCard.append(likesP)
+  let likeBtn = document.createElement('button')
+  likeBtn.className = 'like-btn'
+  likeBtn.innerText = 'Like'
+  likeBtn.addEventListener('click', e => increaseLikes(e, toyNew))
+  toyCard.append(likeBtn)
+  toyCollection.append(toyCard)
+}
+
+const increaseLikes = (e, toy) => {
+  let toyLikes = toy.likes
+  let toyId = toy.id
+  return fetch(`http://localhost:3000/toys/${toyId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ likes: ++toyLikes })
+  })
+    .then(resp => resp.json())
+    .then(showLikes(e))
+}
+
+const showLikes = e => {
+  e.preventDefault()
+  // let num =  e.target.parentElement.children[2].innerText
+  let newNum =
+    parseInt(e.target.parentElement.children[2].innerText.split(' ')[0]) + 1
+  e.target.parentElement.children[2].innerText = `${newNum} Likes`
+}
+initialize()
